506b292b0c49e439cb8574821f18f958b50ae2bf
alder <alder.gamestudio@gmail.com>
2014-06-29 21:41:39 +0200

    Searching for Terry

--- js/event/terry.js
+++ js/event/terry.js
@@ -37,10 +37,19 @@ function Terry(storage) {
 	
 	this.flags["Met"] = 0;
 	
+	this.hidingSpot = world.loc.Rigard.ShopStreet.street;
+	
 	if(storage) this.FromStorage(storage);
 }
 Terry.prototype = new Entity();
 Terry.prototype.constructor = Terry;
+
+Terry.Met = {
+	NotMet : 0,
+	Found  : 1,
+	Caught : 2,
+	Saved  : 3
+};
 
 Terry.prototype.FromStorage = function(storage) {
 	this.Butt().virgin       = parseInt(storage.virgin) == 1;
@@ -54,7 +63,7 @@ Terry.prototype.FromStorage = function(storage) {
 	for(var flag in storage.sex)
 		this.sex[flag] = parseInt(storage.sex[flag]);
 		
-	if(this.flags["Met"] != 0) {
+	if(this.flags["Met"] >= Terry.Met.Caught) {
 		this.name = "Terry";
 		this.avatar.combat = Images.terry_c;
 	}
@@ -76,15 +85,190 @@ Terry.prototype.ToStorage = function() {
 Scenes.Terry = {};
 
 Scenes.Terry.ExploreGates = function() {
+	var parse = {
+		
+	};
 	
+	Text.Clear();
+	Text.Add("With a nod of agreement to each other, you and Miranda start investigating the area, looking for potential hiding spots or clues that could help you catch this thief.", parse);
+	Text.NL();
+	if(terry.flags["Met"] >= Terry.Met.Found) {
+		Text.Add("As best you can, the pair of you make your way through the crowds, looking for the slightest sign of the thief you're chasing, eyes ever alert for a telltale vulpine form. With the sheer number of people here, it doesn't make your task easy, and you keep having to push your way through the scrum.", parse);
+		Text.NL();
+		if(terry.hidingSpot == world.loc.Rigard.Gate) {
+			Text.Add("Your search finally pays off when you see a vulpine tail rounding a corner towards an alleyway. You signal to Miranda and she opens a path in the crowd so you can give chase. As soon as she notices she’s being followed she makes a mad dash towards the other side. <i>”Dammit!</i> Miranda curses as she rushes ahead. You follow in tow.", parse);
+			Text.NL();
+			Text.Add("After a while she finally makes a mistake and rounds a corner on a dead end. Without so much a batting an eye she readies herself for combat!", parse);
+			Text.Flush();
+			
+			Scenes.Terry.CombatVsMiranda();
+			return;
+		}
+		else {
+			Text.Add("In the end you come back empty-handed. Wherever the vixen is, she doesn’t seem to be here.", parse);
+			Text.NL();
+			Text.Add("<i>”Come on, let’s look somewhere else,”</i> Miranda says in annoyance, pushing a path open in the crows so the two of you can get out.", parse);
+		}
+	}
+	else {
+		Text.Add("Despite your exhaustive efforts at searching, it all comes to naught - there isn't a single trace of a clue to be found here. Eventually, Miranda declares it's time to look somewhere else.", parse);
+	}
+	Text.Flush();
+	world.TimeStep({minute : 30});
+	
+	Gui.NextPrompt();
 }
 Scenes.Terry.ExploreResidential = function() {
+	var parse = {
+		
+	};
 	
+	Text.Clear();
+	Text.Add("With a nod of agreement to each other, you and Miranda start investigating the area, looking for potential hiding spots or clues that could help you catch this thief.", parse);
+	Text.NL();
+	
+	if(terry.flags["Met"] >= Terry.Met.Found) {
+		Text.Add("You decide to look around and ask a few people. Someone might’ve seen her. ", parse);
+		if(terry.hidingSpot == world.loc.Rigard.Residental.street) {
+			Text.Add("After a long string of complaints, annoyed comments and plain rudeness, one of the residents finally provides a lead.<i>”A vixen? You mean that one?”</i> they point towards an alleyway, where you see a distinct vulpine running off.", parse);
+			Text.NL();
+			Text.Add("Without missing a beat you call for Miranda and make a mad dash after the thief. You chase after her for a while, until Miranda manages to corner her at a dead end. She draws her blade and prepares for battle!", parse);
+			Text.Flush();
+			
+			Scenes.Terry.CombatVsMiranda();
+			return;
+		}
+		else {
+			Text.Add("After a long string of complaints, annoyed comments and plain rudeness, Miranda approaches you. <i>”Any luck?”</i>", parse);
+			Text.NL();
+			Text.Add("You shake your head.", parse);
+			Text.NL();
+			Text.Add("<i>”Dammit! When I catch that thief...”</i> she trails off into a growl, signalling you to follow.", parse);
+		}
+	}
+	else {
+		Text.Add("You do your best to search, questioning people if they have seen anything strange and poking your nose into any likely looking corner, but in the end, you come up empty-handed. Looking towards Miranda, she shakes her head with a disgusted grimace; evidently her luck was no better than yours. It looks like your thief isn't here.", parse);
+	}
+	Text.Flush();
+	world.TimeStep({minute : 30});
+	
+	Gui.NextPrompt();
 }
 Scenes.Terry.ExploreMerchants = function() {
+	var parse = {
+		weapon : player.WeaponDesc()
+	};
 	
+	Text.Clear();
+	Text.Add("With a nod of agreement to each other, you and Miranda start investigating the area, looking for potential hiding spots or clues that could help you catch this thief.", parse);
+	Text.NL();
+	
+	if(terry.flags["Met"] >= Terry.Met.Found) {
+		if(terry.hidingSpot == world.loc.Rigard.ShopStreet.street) {
+			Text.Add("You and Miranda wander through the warehouses of the merchant’s district, looking for any sign of the sleek vixen. The two of you check a few of them before you catch a glimpse of a moving shadow. Without thinking you rush ahead, Miranda following after you, and as soon as round the corner you’re faced with the vixen thief, already ready for combat!", parse);
+			Text.Flush();
+			Scenes.Terry.CombatVsMiranda();
+			return;
+		}
+		else {
+			Text.Add("Though you and Miranda search the many warehouses, you find no sign of the vulpine thief. It appears she hasn't returned here since you flushed her out before.", parse);
+		}
+	}
+	else {
+		Text.Add("As you consider your options for searching the place, you note it's unlikely that a thief would be hiding in one of the stores. Turning to the long-term resident, you ask Miranda if she has any opinions on where would be likely prospects for ‘good hiding spots’ here.", parse);
+		Text.NL();
+		Text.Add("Miranda shrugs. <i>”There’s always the warehouses. Not much movement around there even during normal days.”</i>", parse);
+		Text.NL();
+		Text.Add("You opinion to her that it would probably be best to try searching the warehouses first, in that case - after all, this thief isn't likely to be hiding themselves in one of the stores.", parse);
+		Text.NL();
+		Text.Add("<i>”Right, this way.”</i>", parse);
+		Text.Flush();
+		
+		Gui.NextPrompt(function() {
+			Text.Clear();
+			Text.Add("Despite your efforts, so far the search has been for nothing; you're both empty-handed despite how thoroughly you keep checking. You are just about to leave the warehouse district and search elsewhere when you spot something; a warehouse with its doors ajar. Recalling Miranda said there isn't much activity here even when things are normal, you deem that suspicious and call her attention to it, suggesting that you should both check it out.", parse);
+			Text.NL();
+			Text.Add("Miranda boldly walks up to the door and kicks it open. <i>”Hey! Is the bastard that stole Krawitz stuff here?”</i>", parse);
+			Text.NL();
+			Text.Add("...That's Miranda for you. ", parse);
+			if(player.SubDom() > 0)
+				Text.Add("She really wouldn't know subtlety if it bit her on the ass, would she?", parse);
+			else
+				Text.Add("There are times when she's a little too direct, even for your taste.", parse);
+			Text.Add(" Much to your surprise, you hear a gasp and the sound of metal hitting the floor.", parse);
+			Text.NL();
+			Text.Add("<i>”Get your weapon ready,”</i> Miranda snarls, taking her sword in her hands and assuming a battle stance. You follow her lead as Miranda shouts, <i>”Show yourself!”</i>", parse);
+			Text.NL();
+			Text.Add("The two of you wait patiently, but when no reply comes Miranda takes a step forward. Immediately you note a small sphere flying towards her. She has no time to reach as the sphere bursts open into a cloud of dust, temporarily blinding the canine guard. <i>”Shit!”</i> she exclaims trying to shake off the dust.", parse);
+			Text.NL();
+			Text.Add("Thankfully you manage to protect your eyes, and by the time you uncover them you’re faced with a blur is headed your way, no doubt making a run for it! You quickly strike them with your [weapon], narrowly missing your mark as the blur takes a step back. Their mask comes loose, falling on the ground, as it does so you’re faced with a familiar face. It’s the vixen from the Lady’s Blessing!", parse);
+			Text.NL();
+			Text.Add("She's traded her uniform for a practical, tight-fitting suit of leather armor. A hood rises from the neck to cover her scalp and partially obscure her features, its long sleeves and pant-legs reaching to her wrists and ankles, but tight against the limbs so as to not get in the way. Bracers and pads add a little extra protection, and the front sports a number of pockets and a holster covered in pouches wrapped diagonally around her chest. All in all, perfect gear for a thief.", parse);
+			Text.NL();
+			Text.Add("<i>”Dammit!”</i> she yells, grabbing a dagger and entering her battle stance.", parse);
+			Text.NL();
+			Text.Add("<i>”Alright asshole, it’s personal now,”</i> Miranda growls as she steps by your side, eyes red from the thief’s initial attack.", parse);
+			Text.Flush();
+			Scenes.Terry.CombatVsMiranda();
+			return;
+		});
+		
+		
+		terry.flags["Met"] = Terry.Met.Found;
+		
+		return;
+	}
+	
+	Text.Flush();
+	world.TimeStep({minute : 30});
+	
+	Gui.NextPrompt();
 }
 Scenes.Terry.ExplorePlaza = function() {
+	var parse = {
+		playername : player.name
+	};
+	
+	Text.Clear();
+	
+	if(terry.flags["Met"] >= Terry.Met.Found) {
+		Text.Add("With a nod of agreement to each other, you and Miranda start investigating the area, looking for potential hiding spots or clues that could help you catch this thief.", parse);
+		Text.NL();
+		Text.Add("Luckily for you, the bustling movement of the people here makes the plaza seem more crowded than it really is, and there aren't that many places to hide anyway. Thusly, if the thief is here, you have a chance of finding her.", parse);
+		Text.NL();
+		if(terry.hidingSpot == world.loc.Rigard.Plaza) {
+			Text.Add("As you make your way through the crowds, you feel someone walk straight into you, having been looking over their shoulder and not watching where they were going. As you shake your head to recover, you find yourself looking right into the eyes of the vixen you were chasing! She yelps in shock and tries to run away, but the crowd is in the path and so she is cornered inadvertently by the scrum. You shout at her to halt, and she replies by drawing her weapons, sending the crowd fleeing and bringing Miranda running to assist.", parse);
+			Text.Flush();
+			Scenes.Terry.CombatVsMiranda();
+			return;
+		}
+		else {
+			Text.Add("You find yourself bumped, shoved, sworn at, shouted over and generally given the run around as you try fording through the seething crowd of people. Eventually, you fight your way free of the crowd and find Miranda quickly joining you, the doberherm watchdog visibly growling in frustration as you shake your head. Evidently, you'll need to try searching elsewhere.", parse);
+		}
+	}
+	else {
+		if(miranda.Attitude() >= Miranda.Attitude.Neutral) {
+		Text.Add("<i>”[playername], this place is already packed with guards. Do you really think a thief would hide here where everyone can see them?”</i> she asks you with obvious disdain.", parse);
+		Text.NL();
+		Text.Add("Even a cursory glance around makes you agree with Miranda's opinion, and you nod your head as you tell her so.", parse);
+		Text.NL();
+		Text.Add("<i>”Then let’s look elsewhere.”</i>", parse);
+		}
+		else {
+			Text.Add("<i>”Use your head and think for once, this place is already packed with guards, plus there’s nowhere to hide. A thief wouldn’t dream of attempting to stay incognito here.”</i>", parse);
+			Text.NL();
+			Text.Add("It's hardly necessary to look to see that Miranda does have a valid point, and you waste no time in agreeing with her that it'd be better to try searching elsewhere.", parse);
+			Text.NL();
+			Text.Add("<i>”Let’s get out of here.”</i>", parse);
+		}
+	}
+	Text.Flush();
+	world.TimeStep({minute : 30});
+	
+	Gui.NextPrompt();
+}
+
+Scenes.Terry.CombatVsMiranda = function() {
 	
 }
 
