b68a5aad11a6321d31b5b19b5383b2257bd871a5
alder <alder.gamestudio@gmail.com>
2014-07-20 12:09:48 +0200

    Changelog/credits

--- js/credits.js
+++ js/credits.js
@@ -26,6 +26,7 @@ CreditsScreen = function() {
 	Text.AddOutput("Some of Miranda's sex scenes: LukaDoc and QuietBrowser<br/>");
 	Text.AddOutput("Kyna Moran: Reaper<br/>");
 	Text.AddOutput("Terry: LukaDoc and QuietBrowser<br/>");
+	Text.AddOutput("Cale: Alder, LukaDoc and QuietBrowser<br/>");
 	
 	Text.Newline();
 	
