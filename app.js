87f2ff97a84a2a22f23ae4e61b90ae8a8cdb2804
alder <alder.gamestudio@gmail.com>
2014-08-03 12:54:42 +0200

    Upped version

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ windowHeight = 700;
 APP_NAME = "Fall of Eden";
 VERSION_MAJOR = 0;
 VERSION_MINOR = 3;
-VERSION_SUBSCRIPT = "k4";
-VERSION_NAME = "Cale the wolf: fixes round 3";
+VERSION_SUBSCRIPT = "k5";
+VERSION_NAME = "Cale the wolf: fixes round 4";
 
 SAVE_VERSION = 19;
 // Save version 1: Initial
