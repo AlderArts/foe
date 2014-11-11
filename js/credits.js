b33f17dabb876a8ea98453f37c1a19fefe1a0f6e
alder <alder.gamestudio@gmail.com>
2014-11-11 06:20:52 +0100

    Patreon credits

--- js/credits.js
+++ js/credits.js
@@ -39,7 +39,7 @@ CreditsScreen = function() {
 	Text.NL();
 	Text.Add(Text.BoldColor("Loyal patreons:"));
 	Text.NL();
-	Text.Add("StarcraftJunkie, Dane, Mark, Marco Zijlmans, Xcuphra, Eric Borrowman, Matthew Sellers, Xxeryon, Lasse, Sagara Saito, Sindri Myr, Timothy Rice");
+	Text.Add("StarcraftJunkie, Dane, Mark, Marco Zijlmans, Xcuphra, Eric Borrowman, Matthew Sellers, Xxeryon, Lasse, Sagara Saito, Sindri Myr, Timothy Rice, Mark Hawker");
 	Text.NL();
 	Text.Add("...and all the rest of you!");
 	Text.Flush();
