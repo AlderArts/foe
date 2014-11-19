d06ae17d904023981891ac475d748cb2ef32a439
alder <alder.gamestudio@gmail.com>
2014-11-19 20:26:01 +0100

    app, 0.3q

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ windowHeight = 700;
 APP_NAME = "Fall of Eden";
 VERSION_MAJOR = 0;
 VERSION_MINOR = 3;
-VERSION_SUBSCRIPT = "p";
-VERSION_NAME = "The Burrows pt.1";
+VERSION_SUBSCRIPT = "q";
+VERSION_NAME = "Terry vaginal";
 
 SAVE_VERSION = 19;
 // Save version 1: Initial
