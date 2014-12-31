7a4ff6dfe05290d456ff6a312652f16a049013ec
kspriv <kspriv@users.noreply.github.com>
2014-12-31 17:16:18 -0600

    Added Weapon type to weapons.

--- js/items/weapons.js
+++ js/items/weapons.js
@@ -1,6 +1,6 @@
 Items.Weapons = {};
 
-Items.Weapons.Dagger = new Item("dag0", "Dagger");
+Items.Weapons.Dagger = new Item("dag0", "Dagger", ItemType.Weapon);
 Items.Weapons.Dagger.price = 15;
 Items.Weapons.Dagger.sDesc = function() { return "dagger"; }
 Items.Weapons.Dagger.lDesc = function() { return "a simple dagger"; }
@@ -11,7 +11,7 @@ Items.Weapons.Dagger.effect.atkMod  = 0.1;
 Items.Weapons.Dagger.effect.dexterity = 1;
 Items.Weapons.Dagger.effect.apPierce = 1;
 
-Items.Weapons.ShortSword = new Item("swrd0", "S.Sword");
+Items.Weapons.ShortSword = new Item("swrd0", "S.Sword", ItemType.Weapon);
 Items.Weapons.ShortSword.price = 50;
 Items.Weapons.ShortSword.sDesc = function() { return "short sword"; }
 Items.Weapons.ShortSword.lDesc = function() { return "a simple short sword"; }
@@ -22,7 +22,7 @@ Items.Weapons.ShortSword.effect.atkMod = 0.1;
 Items.Weapons.ShortSword.effect.strength = 1;
 Items.Weapons.ShortSword.effect.apSlash = 1;
 
-Items.Weapons.KrawitzSword = new Item("swrd1", "K.Sword");
+Items.Weapons.KrawitzSword = new Item("swrd1", "K.Sword", ItemType.Weapon);
 Items.Weapons.KrawitzSword.price = 2000;
 Items.Weapons.KrawitzSword.sDesc = function() { return "fine rapier"; }
 Items.Weapons.KrawitzSword.lDesc = function() { return "Krawitz' fine rapier"; }
@@ -34,7 +34,7 @@ Items.Weapons.KrawitzSword.effect.strength  = 3;
 Items.Weapons.KrawitzSword.effect.dexterity = 10;
 Items.Weapons.KrawitzSword.effect.apPierce  = 1;
 
-Items.Weapons.GreatSword = new Item("swrd2", "G.Sword");
+Items.Weapons.GreatSword = new Item("swrd2", "G.Sword", ItemType.Weapon);
 Items.Weapons.GreatSword.price = 100;
 Items.Weapons.GreatSword.sDesc = function() { return "greatsword"; }
 Items.Weapons.GreatSword.lDesc = function() { return "a large greatsword"; }
@@ -45,7 +45,7 @@ Items.Weapons.GreatSword.effect.atkMod = 1;
 Items.Weapons.GreatSword.effect.strength = 4;
 Items.Weapons.GreatSword.effect.apSlash = 1;
 
-Items.Weapons.Rapier = new Item("swrd3", "Rapier");
+Items.Weapons.Rapier = new Item("swrd3", "Rapier", ItemType.Weapon);
 Items.Weapons.Rapier.price = 75;
 Items.Weapons.Rapier.sDesc = function() { return "rapier"; }
 Items.Weapons.Rapier.lDesc = function() { return "a rapier"; }
@@ -56,7 +56,7 @@ Items.Weapons.Rapier.effect.atkMod    = 0.1;
 Items.Weapons.Rapier.effect.dexterity = 2;
 Items.Weapons.Rapier.effect.apPierce  = 1;
 
-Items.Weapons.WoodenStaff = new Item("staff0", "W.Staff");
+Items.Weapons.WoodenStaff = new Item("staff0", "W.Staff", ItemType.Weapon);
 Items.Weapons.WoodenStaff.price = 15;
 Items.Weapons.WoodenStaff.sDesc = function() { return "wooden staff"; }
 Items.Weapons.WoodenStaff.lDesc = function() { return "a simple wooden staff"; }
@@ -67,7 +67,7 @@ Items.Weapons.WoodenStaff.effect.atkMod = 0;
 Items.Weapons.WoodenStaff.effect.intelligence = 1;
 Items.Weapons.WoodenStaff.effect.apBlunt = 1;
 
-Items.Weapons.LWhip = new Item("whip0", "L.Whip");
+Items.Weapons.LWhip = new Item("whip0", "L.Whip", ItemType.Weapon);
 Items.Weapons.LWhip.price = 30;
 Items.Weapons.LWhip.sDesc = function() { return "leather whip"; }
 Items.Weapons.LWhip.lDesc = function() { return "a simple leather whip"; }
