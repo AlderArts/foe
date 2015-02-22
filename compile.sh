9fd245cfd7c527e9de7f81660a8163348309abf1
alder <alder.gamestudio@gmail.com>
2015-02-22 08:28:15 +0100

    Added outlaws files to payload

--- compile.sh
+++ compile.sh
@@ -128,8 +128,11 @@ java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js \
 	js/event/nomads/cale.js \
 	js/event/nomads/magnus.js \
 	js/event/nomads/cavalcade.js \
+	js/event/outlaws/outlaws.js \
+	js/event/outlaws/aquilius.js \
 	js/event/outlaws/maria.js \
 	js/event/outlaws/cveta.js \
+	js/event/outlaws/bulltower.js \
 	js/event/raven.js \
 	js/event/roaming.js \
 	js/event/citywatch.js \
