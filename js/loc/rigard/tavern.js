a30687c4be7e2c5759ce013d3cd399ba442ac749
alder <alder.gamestudio@gmail.com>
2015-05-15 21:59:14 +0200

    locked Kyna to debug mode

--- js/loc/rigard/tavern.js
+++ js/loc/rigard/tavern.js
@@ -26,7 +26,8 @@ world.loc.Rigard.Tavern.common.links.push(new Link(
 
 
 world.loc.Rigard.Tavern.common.onEntry = function() {
-	if(kyna.flags["Met"] == Kyna.MetFlags.NotMet && world.time.hour > 17 && Math.random() < 0.2) {
+	// TODO Remove
+	if(DEBUG && kyna.flags["Met"] == Kyna.MetFlags.NotMet && world.time.hour > 17 && Math.random() < 0.2) {
 		Scenes.Kyna.Intro();
 	}
 	else {
