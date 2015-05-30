5ce28fb36dcaf86cddaced4932be53488f2f7569
alder <alder.gamestudio@gmail.com>
2015-05-30 21:07:04 +0200

    Aquilius basic approach.

--- js/event/outlaws/aquilius.js
+++ js/event/outlaws/aquilius.js
@@ -1,6 +1,7 @@
 /*
  * Aquilius, Outlaw Avian healer
  */
+Scenes.Aquilius = {};
 
 function Aquilius(storage) {
 	Entity.call(this);
@@ -19,8 +20,6 @@ function Aquilius(storage) {
 }
 Aquilius.prototype = new Entity();
 Aquilius.prototype.constructor = Aquilius;
-
-Scenes.Aquilius = {};
 
 Aquilius.prototype.FromStorage = function(storage) {
 	this.LoadPersonalityStats(storage);
@@ -47,3 +46,58 @@ Aquilius.prototype.IsAtLocation = function(location) {
 		return (world.time.hour >= 7 && world.time.hour < 22);
 	return false;
 }
+
+Scenes.Aquilius.FirstMeeting = function() {
+	var parse = {
+		playername : player.name
+	};
+	
+	aquilius.flags["Met"] = 1;
+	
+	Text.Clear();
+	Text.Add("The moment you push apart the tent flaps and step into the tent’s expansive confines, it becomes readily apparent what the place is: an infirmary. Several bandaged morphs lie on the cots laid out before you, while a couple more mill amongst them, the latter making sure the former are comfortable.", parse);
+	Text.NL();
+	if(party.InParty(kiakai)) {
+		parse["name"] = kiakai.name;
+		parse["hisher"] = kiakai.hisher();
+		Text.Add("<i>“A place of healing and recovery,”</i> [name] breathes, [hisher] voice almost reverent. <i>“It is good that even in this place, there is-”</i>", parse);
+		Text.NL();
+	}
+	Text.Add("The aura of peace that envelops the infirmary is shattered by a loud, piercing scream from behind a curtain near the back of the tent. Oddly, no one seems very much disturbed by the sudden cry, but you decide to investigate anyway, pushing your way through the cots and peering through a gap in the curtains. From where you stand, you spy a horse and eagle-morph within, the latter working on the former with… yes, with a needle and thread. Well, that would explain the screaming.", parse);
+	Text.NL();
+	Text.Add("<i>“Aria’s tits, Zevran,”</i> the eagle-morph mutters. <i>“Don’t be such a wimp. Hold still and don’t yell so much - other folks need their rest. The sooner I can sew you up, the sooner I can throw you out of here - or do you want a leather strop to bite on so you won’t chew your tongue off?”</i>", parse);
+	Text.NL();
+	Text.Add("The horse-morph mumbles something that you can’t quite make out.", parse);
+	Text.NL();
+	Text.Add("<i>“Good, because I usually only have to get out the leather when I’m doing amputations. Now, this is going to be the last bit… there. You’ll need to come back a few days later to have the stitches removed; I’ll put you on light duty in the meantime. Now… this is going to sting a little.”</i>", parse);
+	Text.NL();
+	Text.Add("The eagle-morph pours something onto a clean cloth and swabs away; the horse morph screams again, louder than the first.", parse);
+	Text.NL();
+	Text.Add("<i>“Psh. All right, now, off with you. Maybe the pain will remind you not to risk bursting those stitches.”</i>", parse);
+	Text.NL();
+	Text.Add("Throwing back the curtains with an elbow, the horse-morph stumbles past you, through the cots, and out of the tent - all while clutching his bloodied arm. Your eyes follow him on his way out, and some of the invalids do the same, craning their necks to watch his passage.", parse);
+	Text.NL();
+	Text.Add("<i>“Well, that one was certainly one of the louder folks of late. And who might you be?”</i>", parse);
+	Text.NL();
+	Text.Add("The eagle-morph’s voice is clear and crisp, and you turn to find him staring intently at you with weathered eyes, dousing his hands with clear, strong-smelling liquid from a small pewter jug. A pair of bloodied gloves lie on the table behind him - no doubt he was wearing them while working on the horse-morph - and the coarse cloth vest he has on over his shirt has a few stains in it, too.", parse);
+	Text.NL();
+	Text.Add("Bit of a messy job he’s doing there. Careful not to appear like you’re staring too much, you introduce yourself.", parse);
+	Text.NL();
+	Text.Add("<i>“[playername]. I’ll remember it. That said, my name is Aquilius. Now, you don’t look very injured to me, so are you ill?”</i>", parse);
+	Text.NL();
+	Text.Add("What? No, you aren’t ill, you just-", parse);
+	Text.NL();
+	Text.Add("<i>“If you’re not injured or ill, then you really don’t have much business in here.”</i> He points up at a sign hanging from one of the tent’s support beams. <i>“And a warning, since you’re a new face. If you’re going to report sick, make sure you’re ill. I can’t stand malingerers.”</i>", parse);
+	Text.NL();
+	Text.Add("Well excuse me, you were just going to introduce yourself-", parse);
+	Text.NL();
+	Text.Add("<i>“Yes, you’ve introduced yourself. I’ve also introduced myself. This has been a very good conversation,”</i> Aquilius replies drolly, wiping his hands before corking the flask and putting it away in a pocket. That done, he draws out a beautifully carved wooden case from his shirt’s breast pocket, and from that, a pipe. With no small satisfaction, he sticks it in his beak and lights up, a sweet scent filling the air as he begins to puff away. <i>“If there’s nothing else, there are others who need my time. Have a good day, but please don’t loiter here.”</i>", parse);
+	Text.NL();
+	Text.Add("With that, he walks away, leaving you to fume.", parse);
+	Text.Flush();
+	
+	world.TimeStep({minute: 30});
+	
+	Gui.NextPrompt();
+}
+
--- js/loc/outlaws.js
+++ js/loc/outlaws.js
@@ -6,7 +6,8 @@
 
 // Create namespace
 world.loc.Outlaws = {
-	Camp : new Event("Outlaws' camp")
+	Camp : new Event("Outlaws' camp"),
+	Infirmary : new Event("Infirmary")
 }
 
 world.SaveSpots["Outlaws"] = world.loc.Outlaws.Camp;
@@ -39,6 +40,14 @@ world.loc.Outlaws.Camp.links.push(new Link(
 	null,
 	function() {
 		MoveToLocation(world.loc.Forest.Outskirts, {hour: 1});
+	}
+));
+
+world.loc.Outlaws.Camp.links.push(new Link(
+	"Infirmary", true, true,
+	null,
+	function() {
+		MoveToLocation(world.loc.Outlaws.Infirmary, {minute: 5});
 	}
 ));
 
@@ -89,5 +98,58 @@ world.loc.Outlaws.Camp.events.push(new Link(
 	null,
 	function() {
 		Scenes.Cveta.Performance();
+	}
+));
+
+
+
+world.loc.Outlaws.Infirmary.description = function() {
+	var parse = {
+		
+	};
+	
+	Text.Add("This large tent is what passes for an infirmary in the outlaws’ camp. As soon as you step through the open flaps, three rows of simple wooden cots - no more than tough cloth stretched out on wooden frames - greet your eyes; thankfully, only a few of them are filled at any one time. Further to the back, makeshift workbenches and shelves for storing herbs, minerals and medications; besides that, a burner that works away without rest. You note that ", parse);
+	
+	var scenes = new EncounterTable();
+	scenes.AddEnc(function() {
+		Text.Add("it’s currently boiling water in a large tin pot, in which the good surgeon’s scalpel, tweezers and a host of other disturbingly sharp tools are immersed.", parse);
+	});
+	scenes.AddEnc(function() {
+		parse["t"] = party.InParty(terry) ? Text.Parse(", causing Terry to wrinkle [hisher] nose in disgust", { hisher: terry.hisher() }) : "";
+		Text.Add("it’s been attached to a flask and a maze of glass tubes, obviously a distillery - spirits go in one end, and purified alcohol comes out the other. The resultant smell that emerges with the steam and fills the air is sharp and harsh[t].", parse);
+	});
+	scenes.AddEnc(function() {
+		Text.Add("the flame has been turned down to slowly warm a crucible filled with fragrant herbs, a pleasant aroma wafting through the air that makes you feel restful and drowsy.", parse);
+	});
+	scenes.Get();
+	
+	Text.NL();
+	Text.Add("A large, prominently displayed sign declares: <i>“If you’re going to report sick, you’d better damn well be sick.”</i>", parse);
+	Text.NL();
+	Text.Add("Another corner has been set up with curtains and a large stone table, complete with restraints cobbled together from metal rings and leather straps. Despite the fact that its surface is smooth and great pains have been taken to keep it clean, numerous bloodstains adorn its surface and sides. A number of depressingly grim implements hang from nearby hooks - a large saw, leather strops, what looks like a sharpened butcher’s cleaver…  there’s little doubt that the curtains are there to spare the bedridden the sight of what goes on within by necessity.", parse);
+	Text.NL();
+	Text.Add("Towards the back, obscured by the shelves and a large bookcase, are Aquilius’ quarters. Although you can’t see them from where you stand, you know they’re as sparse and functional as any of the other outlaws’. A couple of potted, leafy plants have been placed near a window, clearly herbs of some sort; there’s even a damp log on which mushrooms are sprouting in large yellow clumps.", parse);
+	Text.NL();
+	
+	if(world.time.hour >= 7 && world.time.hour < 17)
+		Text.Add("Aquilius’ assistants perform the daily grind of seeing to the needs of the injured and bedridden, while the surgeon himself tends to the ignoble task of seeing to those coming in claiming illness or injury.", parse);
+	else
+		Text.Add("While the surgeon has retired for the night, one of his assistants is still on duty, watching over the infirm by the light of a small, hooded lantern. It’s little wonder that being surgeon for the outlaws is a job that doesn’t guarantee regular rest hours.", parse);
+	Text.NL();
+	Text.Add("Taking a moment to savor the peace the infirmary affords, you consider what you ought to do next.", parse);
+}
+
+world.loc.Outlaws.Infirmary.onEntry = function() {
+	if(aquilius.flags["Met"] == 0)
+		Scenes.Aquilius.FirstMeeting();
+	else
+		PrintDefaultOptions();
+}
+
+world.loc.Outlaws.Infirmary.links.push(new Link(
+	"Outside", true, true,
+	null,
+	function() {
+		MoveToLocation(world.loc.Outlaws.Camp, {minute: 5});
 	}
 ));
