362552098d8759b380654d7b78c43a56ec0d5f92
alder <alder.gamestudio@gmail.com>
2015-07-16 21:36:25 +0200

    Gwendy sex partitioned into a separate file

--- compile.sh
+++ compile.sh
@@ -131,6 +131,7 @@ java -jar compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js \
 	js/event/burrows/ophelia.js \
 	js/event/burrows/vena.js \
 	js/event/farm/gwendy.js \
+	js/event/farm/gwendy-sex.js \
 	js/event/farm/adrian.js \
 	js/event/farm/danie.js \
 	js/event/farm/layla.js \
