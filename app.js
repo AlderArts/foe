5aa76b8c865bc08e62f245fcf8d130ad97893daf
alder <alder.gamestudio@gmail.com>
2015-11-01 11:24:21 +0100

    0.4n2

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ windowHeight = 700;
 APP_NAME = "Fall of Eden";
 VERSION_MAJOR = 0;
 VERSION_MINOR = 4;
-VERSION_SUBSCRIPT = "n";
-VERSION_NAME = "Halloween pt.3";
+VERSION_SUBSCRIPT = "n2";
+VERSION_NAME = "Halloween pt.3-1";
 
 SAVE_VERSION = 28;
 // Save version 1: Initial
