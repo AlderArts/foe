131b6d508ce97594158f88314ee7c60f372ef417
CalmKhaos <ajwesleyjr@outlook.com>
2015-12-22 22:27:00 -0500

    Location tweaks
    Desert, King's Road and Maidens' Bane tweaks.

--- js/loc/rigard/tavern.js
+++ js/loc/rigard/tavern.js
@@ -10,7 +10,7 @@
 world.loc.Rigard.Tavern.common.description = function() {
 	Text.Add("You are in the tavern called the Maidens' Bane. The dim lighting makes it hard to make out details, and the heavy smell of hard alcohol mixed with bile stings your nostrils. Along the bar is a row of stools, a lot of them partly broken or mended - either missing a supporting peg, or the cushion is torn open with the material picked out, making the seat lumpy and hard.");
 	Text.NL();
-	Text.Add("In the main dining area there are tables, most of them covered in the leftover dishes, foods and half-drunk mugs of other patrons. There are spills and stains on the tables that look as though they have been there for several weeks, with no hope of getting cleaned up anytime soon. On the wall opposite of the bar there are booths, where it looks like couples could go for some public privacy. For true privacy, there are a few back rooms deeper inside the tavern.");
+	Text.Add("In the main dining area, there are tables - most of them covered in the leftover dishes, foods and half-drunk mugs of other patrons. There are spills and stains on the tables that look as though they have been there for several weeks, with no hope of getting cleaned up anytime soon. On the wall opposite of the bar, there are booths, where it looks like couples could go for some public privacy. For true privacy, there are a few back rooms deeper inside the tavern.");
 	Text.NL();
 	Text.Add("The omnipresent bartender is a tall equine, perpetually busy with wiping a mug that doesn’t ever seem to get any cleaner.");
 	Text.NL();
