4586ea1278b9a1ee52318a5e007b97406e1fedb8
alder <alder.gamestudio@gmail.com>
2016-01-17 15:49:58 +0100

    0.4q

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ windowHeight = 700;
 APP_NAME = "Fall of Eden";
 VERSION_MAJOR = 0;
 VERSION_MINOR = 4;
-VERSION_SUBSCRIPT = "p";
-VERSION_NAME = "Cass snusnu";
+VERSION_SUBSCRIPT = "q";
+VERSION_NAME = "Highland scouts";
 
 SAVE_VERSION = 28;
 // Save version 1: Initial
