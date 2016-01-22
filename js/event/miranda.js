6f49ddffbef6ca8e84617c76db4a61f21da4a12c
alder <alder.gamestudio@gmail.com>
2016-01-22 22:39:20 +0100

    Miranda talks

--- js/event/miranda.js
+++ js/event/miranda.js
@@ -1728,87 +1728,91 @@ Scenes.Miranda.BarChatOptions = function(options, back) {
 	options.push({ nameStr : "Chat",
 		func : function() {
 			Text.Clear();
+			
 			var scenes = new EncounterTable();
 			scenes.AddEnc(function() {
-				Text.Add("Miranda mentions that she likes fish a lot, going on to dreamily explain great fish dishes that she has cooked over time. Funny, you didn't quite look at her like a gourmet cook.", parse);
+				Text.Add("<i>“I like fish a lot, prolly my favorite food. Good thing I live in a port town, eh?”</i> The dobie goes on to dreamily explain great fish dishes that she has cooked over time. Funny, you didn't quite see her as a gourmet cook.", parse);
+				Text.NL();
+				Text.Add("<i>“I’m not, usually. Way too much work. It’s just… sometimes the drudge of the job gets to you. Each day it’s the same old. Those times, I grab a bottle of booze, an unsuspecting guy or girl to fuck blind, and finish it off with a side of fried fish. Best vacation ever.”</i>", parse);
 			}, 1.0, function() { return true; });
 			scenes.AddEnc(function() {
-				Text.Add("Miranda talks for a while about her biggest hobby, hunting game in the woods. While she usually uses a two-handed blade, she is apparently also really proficient with a bow and arrow. She talks a bit about some of her conquests. You get the feeling that she is searching for something in the forest.", parse);
-			}, 1.0, function() { return true; });
-			scenes.AddEnc(function() {
-				Text.Add("The two of you chat a bit about Rigard. <i>“Well, it's a nice enough place,”</i> Miranda concedes, <i>“the bar is nice, I have a decent job that brings the dough in.”</i> She grins widely. <i>“And though the place might not be as interesting as the feline home town, I have you around now to keep me entertained!”</i>", parse);
-				player.AddLustFraction(0.1);
-				miranda.AddLustFraction(0.1);
-			}, 1.0, function() { return true; });
-			scenes.AddEnc(function() {
-				Text.Add("You ask Miranda about the royal family living in the castle. Her mood darkens immediately, and she spits on the floor besides the table.", parse);
+				Text.Add("<i>“Watch your back if you head to the Highlands,”</i> Miranda warns you, <i>“Ever since kingdom troops pulled out, there’s been nothing but fighting over there. I hear there’s some new warlord pulling his weight around among the tribes. Not to mention, it’s close to the Boneyard, and that’s a place you should <b>really</b> avoid.”</i>", parse);
 				Text.NL();
-				Text.Add("<i>“Rigard may be my home, but don’t think for a second I’ve got anything but scorn for that bunch. Fucking aristocrats...”</i>", parse);
+				Text.Add("You ask her to tell you more.", parse);
 				Text.NL();
-				Text.Add("When you ask her why she thinks that way, she gives you a deadpan stare. <i>“Try living in Rigard for a week as a morph, might give you an idea why I left to be a merc.”</i>", parse);
+				Text.Add("<i>“About the rabid army wandering the hills or the place filled with dragon bones?”</i> She shakes her head. <i>“If I were still with the Black Hounds, I could tell you more about the fighting at least. I hear they have a company there fighting. Dunno on which side, it probably changes from week to week.”</i>", parse);
 				Text.NL();
-				Text.Add("Well, what about the rest of the royals?", parse);
-				Text.NL();
-				Text.Add("<i>“Heh, they are a right depraved bunch,”</i> Miranda laughs, a wicked grin playing across her features. <i>“There is plenty a rumor about our dear queen, and don’t get me started on the kids. It’s common knowledge they hit the sack together, and you’d have to look hard to find a girlier prince than Rani.”</i>", parse);
-				Text.NL();
-				Text.Add("<i>“...Still, I’d tap that,”</i> she adds after a thoughtful pause.", parse);
-			}, 1.0, function() { return true; });
-			scenes.AddEnc(function() {
-				Text.Add("<i>“Watch your back if you head to the mountains,”</i> Miranda warns you, <i>“I've heard that there are bandits hiding out somewhere there, and it is very close to the Boneyard, and that place you should <b>really</b> avoid.”</i>", parse);
-			}, 1.0, function() { return true; });
-			scenes.AddEnc(function() {
-				Text.Add("<i>“There’s been a rather odd rumor going around lately,”</i> Miranda tells you. <i>“Apparently, there have been increased sightings of rabbits roaming the countryside. Don’t look at me like that, I’m not making this shit up!”</i> The dog-morph gives you a light punch on the shoulder.", parse);
-				Text.NL();
-				Text.Add("<i>“They walk on two legs, though they are still pretty small. There have been reports of them attacking in large packs, stealing stuff from unwary travelers. Perhaps they are also involved with the recent disappearances.”</i>", parse);
-				Text.NL();
-				Text.Add("You ask her if she’s ever seen one.", parse);
-				Text.NL();
-				Text.Add("<i>“Yeah, they are pretty common critters, I saw them running around occasionally when I worked as a merc, before I joined the Watch. Usually kept to themselves, living in holes underground. They are dumb as doorposts, not interested in anything but eating and fucking all day long, which makes those reports even stranger.”</i> She grins at you playfully. <i>“Wouldn’t that be the life, eh?”</i>", parse);
-			}, 1.0, function() { return !burrows.Access(); });
-
+				Text.Add("<i>“As for the Boneyard… it’s a valley said to be an old burial ground for dragons. Never been there myself, but I’ve heard the stories. Ask travelers about it and they’ll tell you anything from animated skeletons to giant fire-breathing lizards inhabit the place. Not really one for superstition, but I could tell you this much at least: no one who goes in there comes back to tell the tale. I’d stay clear of it if I were you.”</i>", parse);
+			}, 1.0, function() { return !Scenes.Global.PortalsOpen(); }); //Act 1
 			scenes.Get();
+			
 			Text.Flush();
 			
 			back();
 		}, enabled : true,
 		tooltip : "Just chat for a while."
 	});
-	options.push({ nameStr : "Guard",
+	options.push({ nameStr : "Guards gossip",
 		func : function() {
 			Text.Clear();
-			Text.Add("You ask Miranda about her job as a guard.", parse);
+			Text.Add("You ask Miranda about her job in the City Watch. What’s the hot gossip? Any juicy details on criminals?", parse);
 			Text.NL();
-
-			var scenes = [];
 			
-			scenes.push(function() {
-				Text.Add("<i>“Walking the beat keeps the coin coming, though it isn’t as exciting as mercenary work. Believe it or not, things are usually pretty quiet here.”</i> She shrugs irritably. <i>“I hate doing paperwork, give me a thug to beat up any day of the week.”</i>", parse);
+			var scenes = new EncounterTable();
+			
+			scenes.AddEnc(function() {
+				Text.Add("<i>“Walking the beat keeps the coin coming, though it isn’t as exciting as mercenary work. Believe it or not, things are usually pretty quiet here.”</> She shrugs irritably. <i>“I hate doing paperwork - give me a thug to beat up any day of the week.”</i>", parse);
+				Text.NL();
+				Text.Add("With a city this size, there must always be something, right?", parse);
+				Text.NL();
+				Text.Add("<i>“Sure, once in awhile there’s a knucklehead who just won’t give in quietly… I do enjoy those. Getting to be the exception rather than the norm these days though. Guess rumors spread. Even with those incidents, the paper trail afterward is always messy.”</i>", parse);
 				if(miranda.Relation() >= 25) {
 					Text.NL();
-					Text.Add("She brightens up a bit. <i>“You could always show up at the barracks sometime and keep me company!”</i> she suggests. <i>“You know... take my mind off things for a while,”</i> she grins wickedly.", parse);
+					Text.Add("She brightens up a bit. <i>“You could always show up at the barracks sometime, keep me company!”</i> she suggests. <i>“You know... take my mind off things for a while,”</i> she grins wickedly.", parse);
 					player.AddLustFraction(0.1);
 					miranda.AddLustFraction(0.1);
 				}
-			});
-			scenes.push(function() {
-				Text.Add("She tells you a few tidbits of information about her comrades in the guard and their peculiarities. You are particularly surprised that the gruff wolf who is usually standing guard at the main gates is into writing sleazy erotic poetry, and has quite the following in the female population of the town.", parse);
+			}, 1.0, function() { return true; });
+			scenes.AddEnc(function() {
+				Text.Add("She tells you a few tidbits of information about her comrades in the guard and their peculiarities. You are particularly surprised that the gruff wolf usually standing guard at the main gates is into writing sleazy erotic poetry, and has quite the following in the female population of the town.", parse);
 				Text.NL();
-				Text.Add("<i>“He is way too shy to tell anyone about it though, so he writes under an alias”</i>, she grins. <i>“You didn't hear that from me, though.”</i>", parse);
-			});
-			scenes.push(function() {
+				Text.Add("<i>“He is way too shy to tell anyone about it though, so he writes under an alias”</i>, she grins. <i>“You didn't hear that from me though.”</i>", parse);
+				Text.NL();
+				Text.Add("Any particular style he specializes in?", parse);
+				Text.NL();
+				Text.Add("<i>“It’s mostly romance schlock, not really my thing. I tried reading it once, but I got kinda bored. Could use some more action, if you know what I mean.”</i> She winks at you.", parse);
+			}, 1.0, function() { return true; });
+			scenes.AddEnc(function() {
 				Text.Add("She tells you a bit more about her guard troop.", parse);
 				Text.NL();
 				Text.Add("<i>“Did you meet the centaur yet?”</i> she asks you. <i>“He is the strongest guy around here, and a really good archer too.”</i> She brings up a few stories about the two of them hunting together in the forest. Seems like a dependable guy.", parse);
-			});
-			if(miranda.Relation() >= 25) {
-				scenes.push(function() {
-					Text.Add("<i>“Well, it has been a lot more entertaining with you around, I'll tell you that!”</i> she giggles. <i>“The other guys there are complaining that my mind is not on the job any more, due to... distractions,”</i> she grins as you blush faintly.", parse);
-					Text.NL();
-					Text.Add("<i>“It's not a problem though, I can do this job in my sleep... not that you ever let me sleep, honey,”</i> she places a big sloppy kiss on your cheek.", parse);
-					player.AddLustFraction(0.1);
-					miranda.AddLustFraction(0.1);
-				});
-			}
+			}, 1.0, function() { return true; });
+			scenes.AddEnc(function() {
+				Text.Add("<i>“Tell the truth, the most action there’s been recently is that whole mess with Krawitz. That fox was damn slippery, that’s for sure.”</i> Miranda looks irritated. <i>“That thing with Preston after snagged away all the joy I could get from it, though. The Royal Guard always finds a way to shove their feet in where they aren’t wanted and claim glory for work they haven’t done.”</i>", parse);
+				Text.NL();
+				Text.Add("She sighs, taking another draft of her drink.", parse);
+				Text.NL();
+				Text.Add("<i>“Not that they would’ve showed up at all if a noble wasn’t involved. Heck, at this point I doubt they’d bat an eye even if the city stood in flames. If the right person gave the orders, they’d probably be the ones lighting the matches. Whole damn organization is rotten to the core.”</i>", parse);
+			}, 1.0, function() { return rigard.RoyalAccess() && !terry.Recruited(); }); //Finished Krawitz, but didn't recruit Terry yet
+			scenes.AddEnc(function() {
+				Text.Add("<i>“There’s been some ugly rumors about that brothel, the Shadow Lady.”</i> Miranda frowns. <i>“Nothing concrete, but word on the street is that some of their services aren’t exactly legal. People who go there often… change. Not that it’s likely to come to anything... the place is off limits to the Watch.”</i>", parse);
+				Text.NL();
+				Text.Add("How come? It sounds like if there’s precedent, it’d at least warrant an investigation.", parse);
+				Text.NL();
+				Text.Add("<i>“Yeah… not going to happen. The management seems fishy to me, but they have connections in high places. Possibly because of the customers. The kind of customers that bring an escort of Royal Guards on their outings to the whorehouses, if you catch my drift.”</i>", parse);
+				Text.NL();
+				if(belinda.Met())
+					Text.Add("What with her sister working at the brothel, you can understand that she’d be worried… Miranda seems to be very protective of Belinda, despite their estrangement.", parse);
+				else
+					Text.Add("For some reason, she looks really bothered about the whole thing, but you decide to not press the issue.", parse);
+			}, 1.0, function() { return !Scenes.Global.PortalsOpen(); });  //Act 1
+			scenes.AddEnc(function() {
+				Text.Add("<i>“Well, it has been a lot more entertaining with you around, I'll tell you that!”</i> she giggles. <i>“The other guys there are complaining that my mind is not on the job any more, due to... distractions,”</i> she grins as you blush faintly.", parse);
+				Text.NL();
+				Text.Add("<i>“It's not a problem though, I can do this job in my sleep... not that you’d ever let me sleep, honey,”</i> she places a big sloppy kiss on your cheek.", parse);
+				player.AddLustFraction(0.1);
+				miranda.AddLustFraction(0.1);
+			}, 1.0, function() { return miranda.Relation() >= 25; });
 			
 			var sceneId = miranda.flags["RotGuard"];
 			if(sceneId >= scenes.length) sceneId = 0;
@@ -1822,7 +1826,7 @@ Scenes.Miranda.BarChatOptions = function(options, back) {
 			
 			back();
 		}, enabled : true,
-		tooltip : "Ask her about her job as a guardswoman."
+		tooltip : "Ask her what’s new with the City Watch."
 	});
 	options.push({ nameStr : "Forest",
 		func : function() {
