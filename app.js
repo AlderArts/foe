544c4269fec7492602c6281c6e3a1dc8da3f1408
alder <alder.gamestudio@gmail.com>
2016-03-13 14:42:37 +0100

    0.4s

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ windowHeight = 700;
 APP_NAME = "Fall of Eden";
 VERSION_MAJOR = 0;
 VERSION_MINOR = 4;
-VERSION_SUBSCRIPT = "r";
-VERSION_NAME = "Lagon defeated; revamped";
+VERSION_SUBSCRIPT = "s";
+VERSION_NAME = "Malice scouts";
 
 SAVE_VERSION = 29;
 // Save version 1: Initial
