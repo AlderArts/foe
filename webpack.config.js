88471bd8cdb3f14da24f2895e360de4b652b4a4f
alder <alder.gamestudio@gmail.com>
2019-07-16 21:03:21 +0200

    Initial add of webpack.

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './index.js',
+  devtool: 'inline-source-map',
+  plugins: [
+    new CopyWebpackPlugin(['./foe.html']),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|webp)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin({}),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  mode: "development",
+  output: {
+    filename: 'foe.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
