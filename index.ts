7b6d9cd74e028559cb6b76e0849f7f14b42715c8
alder <alder.gamestudio@gmail.com>
2019-08-13 17:39:20 +0200

    Tweaked what files are included in index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 // Assets
-import './js/vendor/kimberley_bl_900.min.font.js';
-
+//import './js/vendor/kimberley_bl_900.min.font.js';
+/*
 import './assets/css/style.css';
 import './assets/css/output.css';
 import './assets/css/droid-sans.css';
+*/
 
 // Start up the main text
 import './js/exploration';
