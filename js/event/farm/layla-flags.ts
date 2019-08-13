aa5cc3c4587a35a2ac25057acd015355051617a6
alder <alder.gamestudio@gmail.com>
2019-08-13 02:14:01 +0200

    namespaced Layla flags

--- js/event/farm/layla-flags.ts
+++ js/event/farm/layla-flags.ts
@@ -1,17 +1,15 @@
-let LaylaFlags = {
-    Met : {
-        NotMet : 0,//Never seen
-        First  : 1,//Met at least once, not defeated
-        Won    : 2,//Defeated
-        Farm   : 3,//Talked to at farm, not in party
-        Party  : 4,//Recruited to party
-        Talked : 5 //Talked to her in party
-    },
+export namespace LaylaFlags {
+    export enum Met {
+        NotMet = 0,//Never seen
+        First  = 1,//Met at least once, not defeated
+        Won    = 2,//Defeated
+        Farm   = 3,//Talked to at farm, not in party
+        Party  = 4,//Recruited to party
+        Talked = 5 //Talked to her in party
+    }
 
-    Talk : {
-        Sex    : 1, //Talked about sex
-        Origin : 2
-    },
-};
-
-export { LaylaFlags };
+    export enum Talk {
+        Sex    = 1, //Talked about sex
+        Origin = 2
+    }
+}
