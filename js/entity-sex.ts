d1fdc5bdff5566081167dbe559dec04c2a4b1680
alder <alder.gamestudio@gmail.com>
2019-09-21 20:41:38 +0200

    applying strict typing, sex functions

--- js/entity-sex.ts
+++ js/entity-sex.ts
@@ -1,24 +1,26 @@
+import { Entity } from "./entity";
+
 /*
  * New Sex functions
  */
 export namespace Sex {
-	export function Cunnilingus(giver: any, reciever: any) {
+	export function Cunnilingus(giver: Entity, reciever: Entity) {
 		if (giver) {    giver.sex.gCunn++; }
 		if (reciever) { reciever.sex.rCunn++; }
 	}
-	export function Blowjob(giver: any, reciever: any) {
+	export function Blowjob(giver: Entity, reciever: Entity) {
 		if (giver) {    giver.sex.gBlow++; }
 		if (reciever) { reciever.sex.rBlow++; }
 	}
-	export function Vaginal(giver: any, reciever: any) {
+	export function Vaginal(giver: Entity, reciever: Entity) {
 		if (giver) {    giver.sex.gVag++; }
 		if (reciever) { reciever.sex.rVag++; }
 	}
-	export function Anal(giver: any, reciever: any) {
+	export function Anal(giver: Entity, reciever: Entity) {
 		if (giver) {    giver.sex.gAnal++; }
 		if (reciever) { reciever.sex.rAnal++; }
 	}
-	export function Preg(father: any, mother: any, num?: number) {
+	export function Preg(father: Entity, mother: Entity, num?: number) {
 		num = num || 1;
 		if (father) { father.sex.sired += num; }
 		if (mother) { mother.sex.birth += num; }
