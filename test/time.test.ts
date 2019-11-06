944d4c89ee5216a1eb600fe21e03e3cbbcce1b26
alder <alder.gamestudio@gmail.com>
2019-11-06 21:32:39 +0100

    Added testing commands. Doesn't support all files yet (things with jQuery will fail)

---
+++ test/time.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { Season } from "../js/time";
+
+describe("Season", () => {
+    it("test test", () => {
+        expect(Season.Spring === 0).to.be.true;
+    });
+});
