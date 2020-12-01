1c3ec7aa3c1b6d3992865128ff7e92adc67ad571
alder <alder.gamestudio@gmail.com>
2020-12-01 08:20:36 +0100

    Adding debug scenes variable. Need to fill in manually.

---
+++ js/SCENES.ts
@@ -0,0 +1,11 @@
+import { AscheScenes } from "./event/asche-scenes";
+import { AscheTasksScenes } from "./event/asche-tasks";
+
+const scenes = {
+    AscheScenes,
+    AscheTasksScenes,
+};
+
+export function SCENES() {
+    return scenes;
+}
--- js/debug.js
+++ js/debug.js
@@ -1,6 +1,8 @@
 import { GAME, WORLD } from "./GAME";
+import { SCENES } from "./SCENES";
 
 export function InitDebugObjects() {
     window.game = GAME();
     window.world = WORLD();
+    window.scenes = SCENES();
 }
