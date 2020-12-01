bc16a8a596acbb51dc364611e32e46f3857152aa
alder <alder.gamestudio@gmail.com>
2020-12-01 18:03:49 +0100

    Added all non-grouped event namespaces to SCENESAdded all non-grouped event namespaces to SCENES.

--- js/SCENES.ts
+++ js/SCENES.ts
@@ -1,9 +1,51 @@
+// import { AriaScenes } from "./event/aria";
 import { AscheScenes } from "./event/asche-scenes";
+import { AscheSexScenes } from "./event/asche-sex";
 import { AscheTasksScenes } from "./event/asche-tasks";
+import { CassidyScenes } from "./event/cassidy-scenes";
+import { CassidySexScenes } from "./event/cassidy-sex";
+import { DreamsScenes } from "./event/dreams";
+import { FeraScenes } from "./event/fera";
+import { HalloweenScenes } from "./event/halloween";
+import { Intro } from "./event/introduction";
+import { KiakaiScenes } from "./event/kiakai-scenes";
+import { KiakaiSexScenes } from "./event/kiakai-sex";
+import { MasturbationScenes } from "./event/masturbation";
+import { MeditationScenes } from "./event/meditation";
+import { MirandaScenes } from "./event/miranda-scenes";
+import { MomoScenes } from "./event/momo";
+import { NurseryScenes } from "./event/nursery";
+import { PoetScenes } from "./event/poet";
+import { PortalOpeningScenes } from "./event/portalopening";
+import { RavenMotherScenes } from "./event/raven";
+import { RoamingScenes } from "./event/roaming";
+import { Room69Scenes } from "./event/room69";
+import { TerryScenes } from "./event/terry-scenes";
 
 const scenes = {
+    // AriaScenes,
     AscheScenes,
+    AscheSexScenes,
     AscheTasksScenes,
+    CassidyScenes,
+    CassidySexScenes,
+    DreamsScenes,
+    FeraScenes,
+    HalloweenScenes,
+    Intro,
+    KiakaiScenes,
+    KiakaiSexScenes,
+    MasturbationScenes,
+    MeditationScenes,
+    MirandaScenes,
+    MomoScenes,
+    NurseryScenes,
+    PoetScenes,
+    PortalOpeningScenes,
+    RavenMotherScenes,
+    RoamingScenes,
+    Room69Scenes,
+    TerryScenes,
 };
 
 export function SCENES() {
